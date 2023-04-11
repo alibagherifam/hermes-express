9dda8ff5fd7fe7e4a63225a08a8c52bf7c49dac0
Ali Bagherifam <ali.bagherifam@gmail.com>
2023-04-11 19:49:01 +0430

    Init Koin dependency injection

---
+++ biker-app/src/main/java/dev/alibagherifam/hermesexpress/biker/MainApplication.kt
@@ -0,0 +1,16 @@
+package dev.alibagherifam.hermesexpress.biker
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.GlobalContext.startKoin
+
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@MainApplication)
+            modules(mainModule)
+        }
+    }
+}
---
+++ biker-app/src/main/java/dev/alibagherifam/hermesexpress/biker/MainModule.kt
@@ -0,0 +1,6 @@
+package dev.alibagherifam.hermesexpress.biker
+
+import org.koin.dsl.module
+
+val mainModule = module {
+}
