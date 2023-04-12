94768c8ada439e116d0db575b3a6ded12a698193
Ali Bagherifam <ali.bagherifam@gmail.com>
2023-04-13 03:11:00 +0430

    Add OrderDetails to MainScreen

--- biker-app/src/main/java/dev/alibagherifam/hermesexpress/biker/MainActivity.kt
+++ biker-app/src/main/java/dev/alibagherifam/hermesexpress/biker/MainActivity.kt
@@ -3,59 +3,57 @@ package dev.alibagherifam.hermesexpress.biker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.alibagherifam.hermesexpress.common.theme.AppTheme
+import dev.alibagherifam.hermesexpress.order.domain.generateFakeOrder
+import dev.alibagherifam.hermesexpress.order.ui.OrderDetails
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MainScreen()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String) {
-    Column(Modifier.fillMaxSize()) {
-        Text(
-            text = "Hello $name!",
-            style = MaterialTheme.typography.displayMedium
-        )
-        Text(
-            text = "Hello $name!",
-            style = MaterialTheme.typography.headlineMedium
-        )
-        Text(
-            text = "Hello $name!",
-            style = MaterialTheme.typography.titleMedium
-        )
-        Text(
-            text = "Hello $name!",
-            style = MaterialTheme.typography.bodyMedium
-        )
+fun MainScreen() {
+    val scaffoldState = rememberBottomSheetScaffoldState()
+    BottomSheetScaffold(
+        scaffoldState = scaffoldState,
+        sheetPeekHeight = 160.dp,
+        sheetContent = {
+            OrderDetails(
+                order = generateFakeOrder(),
+                onAcceptOfferClick = { /* finish the app */ }
+            )
+        }
+    ) {
+        Surface(Modifier.fillMaxSize()) {
+        }
+    }
+    LaunchedEffect(key1 = Unit) {
+        scaffoldState.bottomSheetState.expand()
     }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
+fun MainScreenPreview() {
     AppTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
