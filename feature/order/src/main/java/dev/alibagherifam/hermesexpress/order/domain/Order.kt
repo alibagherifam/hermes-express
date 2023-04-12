2c76bd9545986ef8cb38bc451923737e1e4ed3ef
Ali Bagherifam <ali.bagherifam@gmail.com>
2023-04-13 02:35:05 +0430

    Create order domain model

---
+++ feature/order/src/main/java/dev/alibagherifam/hermesexpress/order/domain/Order.kt
@@ -0,0 +1,32 @@
+package dev.alibagherifam.hermesexpress.order.domain
+
+import java.time.Instant
+
+data class Order(
+    val id: Int,
+    val terminals: List<Terminal>,
+    val price: Float,
+    val isBoxRequired: Boolean = true,
+    val reverseLogistics: Boolean = false,
+    val createdAt: Instant,
+    val expireAt: Instant,
+) {
+    init {
+        require(id > 0) { "Order ID should be a positive number" }
+        require(terminals.size > 1) { "Order should contains at least 2 terminals" }
+        require(price > 0f) { "Order Price should be a positive number" }
+        require(expireAt > createdAt) { "Order's expiration should be after its creation" }
+    }
+}
+
+val Order.isExpired: Boolean get() = Instant.now() > expireAt
+val Order.origin: Terminal get() = terminals.first()
+val Order.designations: List<Terminal> get() = terminals.drop(1)
+
+fun generateFakeOrder() = Order(
+    id = 1,
+    generateFakeTerminals(),
+    price = 24.80f,
+    createdAt = Instant.EPOCH,
+    expireAt = Instant.now()
+)
