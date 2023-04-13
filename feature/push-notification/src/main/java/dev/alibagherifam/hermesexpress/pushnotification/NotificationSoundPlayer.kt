71fe1a333786b1e6d87dc710bc2e92ee0c2c645b
Ali Bagherifam <ali.bagherifam@gmail.com>
2023-04-13 04:33:49 +0430

    Add Notification Sound

---
+++ feature/push-notification/src/main/java/dev/alibagherifam/hermesexpress/pushnotification/NotificationSoundPlayer.kt
@@ -0,0 +1,49 @@
+package dev.alibagherifam.hermesexpress.pushnotification
+
+import android.content.Context
+import android.media.MediaPlayer
+import androidx.annotation.RawRes
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
+
+suspend fun playNotificationSound(
+    context: Context,
+    @RawRes soundResId: Int
+) = suspendCancellableCoroutine { continuation ->
+    val mediaPlayer = MediaPlayer.create(context, soundResId)
+    mediaPlayer.setOnCompletionListener {
+        mediaPlayer.release()
+        continuation.resume(Unit)
+    }
+    mediaPlayer.start()
+    continuation.invokeOnCancellation {
+        mediaPlayer.release()
+    }
+}
+
+@Deprecated(message = "Use playNotificationSound() instead")
+class NotificationSoundPlayer(@RawRes private val mediaResId: Int) {
+    private var mediaPlayer: MediaPlayer? = null
+
+    fun prepare(context: Context) {
+        mediaPlayer = MediaPlayer.create(context, mediaResId)
+    }
+
+    fun play() {
+        val player = checkNotNull(mediaPlayer) { "Call prepare() before play()" }
+        player.run {
+            if (isPlaying) {
+                pause()
+            }
+            seekTo(0)
+            start()
+        }
+    }
+
+    fun release() {
+        mediaPlayer?.run {
+            stop()
+            release()
+        }
+    }
+}
