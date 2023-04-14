2aab1b71d037174613decd4099f703243b7b3d7d
Ali Bagherifam <ali.bagherifam@gmail.com>
2023-04-14 21:09:08 +0430

    Remove Point value object

--- core/common/src/main/java/dev/alibagherifam/hermesexpress/common/domain/Point.kt
+++
@@ -1,6 +0,0 @@
-package dev.alibagherifam.hermesexpress.common.domain
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class Point(val latitude: Double, val longitude: Double)
--- core/common/src/main/java/dev/alibagherifam/hermesexpress/common/domain/Terminal.kt
+++ core/common/src/main/java/dev/alibagherifam/hermesexpress/common/domain/Terminal.kt
@@ -4,21 +4,26 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Terminal(
-    val point: Point,
+    val latitude: Double,
+    val longitude: Double,
     val postalAddress: String
 ) {
     init {
-        require(postalAddress.isNotEmpty()) { "Terminal need a postal address" }
+        require(latitude > 0) { "Latitude ID is not allowed" }
+        require(longitude > 0) { "Longitude ID is not allowed" }
+        require(postalAddress.isNotEmpty()) { "Postal address can not be empty" }
     }
 }
 
 fun generateFakeTerminals() = listOf(
     Terminal(
-        Point(60.239, 25.004),
+        latitude = 60.239,
+        longitude = 25.004,
         postalAddress = "Robert Robertson, 1234 NW Bobcat Lane, St. Robert, MO 65584"
     ),
     Terminal(
-        Point(55.004, 39.239),
+        latitude = 55.004,
+        longitude = 39.239,
         postalAddress = "Miranda McAnderson, 6543 N 9th Street, AA 33608-1234"
     )
 )
