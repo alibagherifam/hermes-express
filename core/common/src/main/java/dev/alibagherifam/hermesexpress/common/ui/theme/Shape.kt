2535a4d68d389e14fcc98355851bb0ebf08eebc8
Ali Bagherifam <ali.bagherifam@gmail.com>
2023-04-19 02:18:04 +0430

    Use rounded corner shapes

--- core/common/src/main/java/dev/alibagherifam/hermesexpress/common/ui/theme/Shape.kt
+++ core/common/src/main/java/dev/alibagherifam/hermesexpress/common/ui/theme/Shape.kt
@@ -1,13 +1,13 @@
 package dev.alibagherifam.hermesexpress.common.ui.theme
 
-import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 val AppShapes = Shapes(
-    extraSmall = CutCornerShape(12.dp),
-    small = CutCornerShape(18.dp),
-    medium = CutCornerShape(24.dp),
-    large = CutCornerShape(30.dp),
-    extraLarge = CutCornerShape(36.dp)
+    extraSmall = RoundedCornerShape(4.dp),
+    small = RoundedCornerShape(8.dp),
+    medium = RoundedCornerShape(12.dp),
+    large = RoundedCornerShape(16.dp),
+    extraLarge = RoundedCornerShape(40.dp)
 )
