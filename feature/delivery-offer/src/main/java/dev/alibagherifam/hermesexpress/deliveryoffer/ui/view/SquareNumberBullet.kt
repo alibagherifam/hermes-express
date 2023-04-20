ca217ea4cc241ca1e4a14693e1a5c45099379453
Ali Bagherifam <ali.bagherifam@gmail.com>
2023-04-20 09:05:06 +0430

    Add preview to SquareNumberBullet

--- feature/delivery-offer/src/main/java/dev/alibagherifam/hermesexpress/deliveryoffer/ui/view/SquareNumberBullet.kt
+++ feature/delivery-offer/src/main/java/dev/alibagherifam/hermesexpress/deliveryoffer/ui/view/SquareNumberBullet.kt
@@ -2,12 +2,13 @@ package dev.alibagherifam.hermesexpress.deliveryoffer.ui.view
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -17,7 +18,7 @@ internal fun SquareNumberBullet(
 ) {
     Box(
         modifier
-            .size(width = 20.dp, height = 20.dp)
+            .sizeIn(minWidth = 24.dp, minHeight = 24.dp)
             .background(
                 color = MaterialTheme.colorScheme.onErrorContainer,
                 shape = MaterialTheme.shapes.small
@@ -30,4 +31,10 @@ internal fun SquareNumberBullet(
             color = MaterialTheme.colorScheme.onError
         )
     }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
+@Composable
+internal fun SquareNumberBulletPreview() {
+    SquareNumberBullet(number = 1)
 }
