cc3a9445741543ac99e9eaf28033163da19bfd3d
Ali Bagherifam <ali.bagherifam@gmail.com>
2023-04-23 16:18:00 +0430

    Simplify DI module

--- core/cloud-messaging/src/main/java/dev/alibagherifam/hermesexpress/cloudmessaging/CloudMessagingModule.kt
+++ core/cloud-messaging/src/main/java/dev/alibagherifam/hermesexpress/cloudmessaging/CloudMessagingModule.kt
@@ -1,15 +1,13 @@
 package dev.alibagherifam.hermesexpress.cloudmessaging
 
 import dev.alibagherifam.hermesexpress.common.data.RealTimeDeliveryOfferDatasource
-import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.named
 import org.koin.core.module.dsl.singleOf
-import org.koin.core.module.dsl.withOptions
 import org.koin.dsl.module
 
 val cloudMessagingModule = module {
-    single { CloudMessaging(androidContext()) } withOptions {
+    singleOf(::CloudMessaging) {
         bind<CloudMessagingTokenDatasource>()
     }
 
