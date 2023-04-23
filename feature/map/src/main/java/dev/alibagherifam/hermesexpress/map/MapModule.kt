a5cc10eb2a064a059cb1dabea53108ca5c7e8235
Ali Bagherifam <ali.bagherifam@gmail.com>
2023-04-23 17:02:16 +0430

    Improve Map DI module

--- feature/map/src/main/java/dev/alibagherifam/hermesexpress/map/MapModule.kt
+++ feature/map/src/main/java/dev/alibagherifam/hermesexpress/map/MapModule.kt
@@ -1,8 +1,12 @@
 package dev.alibagherifam.hermesexpress.map
 
 import dev.alibagherifam.hermesexpress.common.data.LocationProvider
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val mapModule = module {
-    single<LocationProvider> { MapBoxLocationProvider() }
+    singleOf(::MapBoxLocationProvider) {
+        bind<LocationProvider>()
+    }
 }
