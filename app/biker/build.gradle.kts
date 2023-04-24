c2a5ec66159f9b56e16145bc0b349c5cf7736f61
Ali Bagherifam <ali.bagherifam@gmail.com>
2023-04-24 11:09:26 +0430

    Prepare for release

--- app/biker/build.gradle.kts
+++ app/biker/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "dev.alibagherifam.hermesexpress.biker"
         versionCode = 1
-        versionName = "0.1.0"
+        versionName = "1.0.0"
     }
 }
 
