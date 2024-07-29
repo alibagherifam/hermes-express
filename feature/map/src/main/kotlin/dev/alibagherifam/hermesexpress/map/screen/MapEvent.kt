b94d6f0fe5b0d48edfd8b83abf08cf322c0c9080
Ali Bagherifam <ali.bagherifam@snapp.cab>
2024-07-29 17:00:52 +0330

    Convert object to data object

--- feature/map/src/main/kotlin/dev/alibagherifam/hermesexpress/map/screen/MapEvent.kt
+++ feature/map/src/main/kotlin/dev/alibagherifam/hermesexpress/map/screen/MapEvent.kt
@@ -3,7 +3,7 @@ package dev.alibagherifam.hermesexpress.map.screen
 import com.mapbox.geojson.Point
 
 internal sealed interface MapEvent {
-    object CameraMovedAccordingly : MapEvent
-    object MarkersUpdated : MapEvent
+    data object CameraMovedAccordingly : MapEvent
+    data object MarkersUpdated : MapEvent
     data class UserLocationChange(val newLocation: Point) : MapEvent
 }
