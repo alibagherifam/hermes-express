9fe9ccc6d14bd96e0eb8fb20bd32e0788a86eeb8
Ali Bagherifam <ali.bagherifam@snapp.cab>
2024-08-22 00:40:34 +0330

    Format gradle scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.detekt) apply false
     alias(libs.plugins.gms) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
 
     alias(libs.plugins.versions)
     alias(libs.plugins.gradleDoctor)
